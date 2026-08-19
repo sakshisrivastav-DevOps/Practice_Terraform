1cf4ec74fee0f4722ba0acfa55e6aa8ff2ecd5c4
Sakshi Srivastav <sakshisrivastavlearn@gmail.com>
2026-08-19 14:13:17 +0530

    Add security group resource definition in Terraform
    Define a security group resource with dynamic ingress rules.

--- aws_module_project/modules/security-group/main.tf
+++ aws_module_project/modules/security-group/main.tf
@@ -1,1 +1,33 @@
 # Security group resource definition
+# modules/security-group/main.tf
+
+resource "aws_security_group" "this" {
+  name        = var.sg_name
+  description = "Security group created through Terraform module"
+  vpc_id      = var.vpc_id
+
+  dynamic "ingress" {
+    for_each = var.ingress_port
+
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(
+    var.tags,
+    {
+      Name = var.sg_name
+    }
+  )
+}
